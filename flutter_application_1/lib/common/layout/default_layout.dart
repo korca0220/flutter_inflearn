741643433a0b5b4f84411fe304ac36de5f5bdbf8
Junewoo <korca02220@naver.com>
2022-10-10 23:00:08 +0900

    add appbar

--- flutter_application_1/lib/common/layout/default_layout.dart
+++ flutter_application_1/lib/common/layout/default_layout.dart
@@ -1,16 +1,46 @@
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
-  const DefaultLayout({required this.child, this.backgroundColor, super.key});
+  const DefaultLayout({
+    required this.child,
+    this.backgroundColor,
+    this.title,
+    this.bottomNavigationbar,
+    super.key,
+  });
 
   final Widget child;
   final Color? backgroundColor;
+  final String? title;
+  final Widget? bottomNavigationbar;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: renderAppbar(),
       body: child,
+      bottomNavigationBar: bottomNavigationbar,
     );
+  }
+
+  AppBar? renderAppbar() {
+    if (title == null) {
+      return null;
+    } else {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: 16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        foregroundColor: Colors.black,
+      );
+    }
   }
 }
--- flutter_application_1/lib/common/view/root_tab.dart
+++ flutter_application_1/lib/common/view/root_tab.dart
@@ -1,15 +1,53 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_application_1/common/const/colors.dart';
 import 'package:flutter_application_1/common/layout/default_layout.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: Center(
+      title: '코팩 딜리버리',
+      bottomNavigationbar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.shifting,
+        onTap: (index) {
+          setState(() {
+            this.index = index;
+          });
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: '프로필',
+          ),
+        ],
+      ),
+      child: const Center(
         child: Text('Root Tab'),
       ),
     );

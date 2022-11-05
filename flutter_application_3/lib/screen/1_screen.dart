23e101a13188a03191d679d46daa72cad4ede071
Junewoo <krca0220@gmail.com>
2022-11-05 14:43:01 +0900

    pop add

--- flutter_application_3/lib/screen/1_screen.dart
+++ flutter_application_3/lib/screen/1_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_application_3/layout/default_layout.dart';
+import 'package:go_router/go_router.dart';
 
 class OneScreen extends StatelessWidget {
   const OneScreen({super.key});
@@ -10,8 +11,14 @@ class OneScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return DefaultLayout(
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-            
+          ElevatedButton(
+            onPressed: () {
+              context.pop();
+            },
+            child: Text('pop'),
+          ),
         ],
       ),
     );

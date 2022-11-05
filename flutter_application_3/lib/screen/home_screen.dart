304e226f7af6b0ea107cf5a89c931afede07ea4d
Junewoo <krca0220@gmail.com>
2022-11-05 15:38:35 +0900

    login,logout add

--- flutter_application_3/lib/screen/home_screen.dart
+++ flutter_application_3/lib/screen/home_screen.dart
@@ -1,13 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_3/layout/default_layout.dart';
+import 'package:flutter_application_3/provider/auth_provider.dart';
 import 'package:flutter_application_3/screen/3_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
       body: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
@@ -24,6 +26,20 @@ class HomeScreen extends StatelessWidget {
               context.goNamed(ThreeScreen.routeName);
             },
             child: const Text('Screen Three (GO)'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // one, two 는 push 하지 않았지만 밑에 깔리게 됨
+              context.go('/login');
+            },
+            child: const Text('Login  Screen (GO)'),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // one, two 는 push 하지 않았지만 밑에 깔리게 됨
+              ref.read(userProvider.notifier).logout();
+            },
+            child: const Text('Logout (GO)'),
           ),
         ],
       ),
--- flutter_application_3/lib/screen/login_screen.dart
+++ flutter_application_3/lib/screen/login_screen.dart
@@ -2,15 +2,25 @@ import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_application_3/layout/default_layout.dart';
+import 'package:flutter_application_3/provider/auth_provider.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends ConsumerWidget {
   const LoginScreen({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     return DefaultLayout(
       body: Column(
-        children: [Text('login')],
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              ref.read(userProvider.notifier).login(name: 'june');
+            },
+            child: const Text('login'),
+          )
+        ],
       ),
     );
   }

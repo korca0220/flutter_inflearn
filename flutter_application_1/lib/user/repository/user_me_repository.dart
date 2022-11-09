300e5b3d17e4163358e3205a68c8d916afc917be
Junewoo <krca0220@gmail.com>
2022-11-09 14:19:41 +0900

    user repositoryt add

---
+++ flutter_application_1/lib/user/repository/user_me_repository.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart' hide Headers;
+import 'package:flutter_application_1/common/const/data.dart';
+import 'package:flutter_application_1/common/dio/dio.dart';
+import 'package:flutter_application_1/user/model/user_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:retrofit/retrofit.dart';
+
+part 'user_me_repository.g.dart';
+
+final userRepositoryProvider = Provider((ref) {
+  final dio = ref.watch(dioProvider);
+  return UserRepository(dio, baseUrl: 'http://$ip/user/me');
+});
+
+@RestApi()
+abstract class UserRepository {
+  factory UserRepository(Dio dio, {String baseUrl}) = _UserRepository;
+
+  @GET('/')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<UserModel> getMe();
+}

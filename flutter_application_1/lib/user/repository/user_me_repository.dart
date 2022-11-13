e7a410909080621413ed47f89a11b07b7aecdaf8
Junewoo <korca02220@naver.com>
2022-11-13 22:53:54 +0900

    add basket item model

--- flutter_application_1/lib/user/repository/user_me_repository.dart
+++ flutter_application_1/lib/user/repository/user_me_repository.dart
@@ -1,6 +1,8 @@
 import 'package:dio/dio.dart' hide Headers;
 import 'package:flutter_application_1/common/const/data.dart';
 import 'package:flutter_application_1/common/dio/dio.dart';
+import 'package:flutter_application_1/user/model/basket_item_model.dart';
+import 'package:flutter_application_1/user/model/patch_basket_body.dart';
 import 'package:flutter_application_1/user/model/user_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:retrofit/retrofit.dart';
@@ -21,4 +23,18 @@ abstract class UserRepository {
     'accessToken': 'true',
   })
   Future<UserModel> getMe();
+
+  @GET('/basket')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<List<BasketItemModel>> getBasket();
+
+  @PATCH('/basket')
+  @Headers({
+    'accessToken': 'true',
+  })
+  Future<List<BasketItemModel>> patchBasket({
+    @Body() required PatchBasketBody body,
+  });
 }
